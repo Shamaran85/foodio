56c9fdd95469a631192ac29acf726bec334676b0
Daniel <shamaran@gmail.com>
2018-10-21 21:30:20 +0200

    Added some layout and some SVG icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import SVGIcon from "./SVGIcon";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="sidebar">
+          <div className="sidebar-top">
+            <p className="sidebar-logo">CMS Panel</p>
+          </div>
+          <div className="sidebar-navigation">
+            <p className="sidebar-navigation-title">Pages</p>
+            <a href="#breakfast"><i class="fas fa-coffee"></i>Breakfast</a>
+            <a href="#lunch"><i class="fas fa-utensils"></i>Lunch</a>
+              <div className="navbar-item">
+              <span>
+                <SVGIcon name="breakfast" width="32px" className="testTwo" />
+                </span>
+              <a href="#dinner"> Dinner </a>
+              </div>
+              <a href="#desert"> Desert </a>
+              <a href="#smoothies"> Smoothies </a>
+
+              <p className="sidebar-navigation-title">Settings</p>
+          </div>
+        </div>
+        <div className="content">
+          <p>Content</p>
+        </div>
       </div>
     );
   }
